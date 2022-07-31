a9e2ee658869fe87311c49bb2198f3eaf9abf0cd
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-07-31 23:55:11 +0100

    Edit Venue completed

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -410,20 +410,22 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
+  venue_row = Venue.query.get(venue_id)
   venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+    "id": venue_row.id,
+    "name": venue_row.name,
+    "genres": [x.name for x in venue_row.genres],
+    "address": venue_row.address,
+    "city": venue_row.city,
+    "state": venue_row.state,
+    "phone": venue_row.phone,
+    "website": venue_row.website,
+    "facebook_link": venue_row.facebook_link,
+    "seeking_talent": venue_row.seeking_talent,
+    "seeking_description": venue_row.seeking_description,
+    "image_link": venue_row.image_link
   }
+  
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -431,6 +433,45 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+  error = False
+
+  # Check whether genre with name exists, else create a new genre
+  genres= []
+  for name in request.form.getlist('genres'):
+    genre = Genre.query.filter_by(name=name).first()
+    if genre:
+      genres.append(genre)
+    else:
+      genres.append(Genre(name=name))
+
+  try:
+    venue = Venue.query.get(venue_id)
+    venue.name = request.form['name']
+    venue.genres = genres
+    venue.address =request.form['address']
+    venue.city = request.form['city']
+    venue.state = request.form['state']
+    venue.phone = request.form['phone']
+    venue.website = request.form['website_link']
+    venue.facebook_link = request.form['facebook_link']
+    venue.seeking_talent = True if 'seeking_talent' in request.form else False
+    venue.seeking_description = request.form['seeking_description']
+    venue.image_link = request.form['image_link']
+
+    db.session.commit()
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    # flash('An error occurred. Venue ' + venue.name + ' could not be edited.')
+    print("error")
+  else:
+    # on successful db insert, flash success
+    # flash('Venue ' + venue.name + ' was successfully listed!')
+    print("error")
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
