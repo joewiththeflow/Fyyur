5e5250590b0d4433db6ec72fccccfa94f80c9818
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-07-31 21:29:22 +0100

    list artists complete

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -310,16 +310,13 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = []
+  for artist in Artist.query.all():
+    data.append({
+      "id": artist.id,
+      "name": artist.name,
+    })
+
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
