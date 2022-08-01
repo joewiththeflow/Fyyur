28ec172469cc8439be6f47907eeb693f358d3aca
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-08-01 07:33:14 +0100

    Search for artists case insensitive completed

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -328,13 +328,18 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_term = request.form.get('search_term', '')
+  data = []
+  for artist in Artist.query.filter(func.lower(Artist.name).contains(func.lower(search_term))):
+    data.append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": len([show for show in artist.shows if show.start_time > datetime.now()]),
+    })
+  
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(data),
+    "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
